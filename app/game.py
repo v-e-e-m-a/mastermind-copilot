874736f8755e99efddc60bc8e59eb1f3d53fbdd4
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2025-07-30 13:29:59 -0700

    Add starting place for refactor & ensuring tests run as expected

--- app/game.py
+++ app/game.py
@@ -1,34 +1,93 @@
+import random
+
+COLORS = {"R", "O", "Y", "G", "B", "P"}
+
+
 # Wave 1
 def generate_code():
-    pass
+    colors_tuple = tuple(COLORS)
+    return [random.choice(colors_tuple) for _ in range(4)]
 
 
 def validate_guess(guess):
-    pass
+    if len(guess) != 4:
+        return False
+    
+    guess_uppercase = get_uppercased_guess(guess)
+    return set(guess_uppercase).issubset(COLORS)
+
+
+def get_uppercased_guess(guess):
+    return [letter.upper() for letter in guess]
 
 
 def check_win_or_lose(guess, code, num_guesses):
-    pass
+    if num_guesses >= 8:
+        return False
+
+    guess_uppercase = get_uppercased_guess(guess)
+    if code == guess_uppercase:
+        return True
 
 
 # Wave 2
 def color_count(guess, code):
-    pass
+    # Create frequency maps of the letters in guess and code
+    guess_map = {}
+    code_map = {}
+    for index in range(4):
+        letter_guess = guess[index].upper()
+        addOrUpdateDictEntry(letter_guess, guess_map)
+
+        letter_code = code[index]
+        addOrUpdateDictEntry(letter_code, code_map)
+
+    # For each unique letter in code, see if it 
+    # exists in guess; if so, increase count. 
+    count = 0
+    for key, code_value in code_map.items():
+        if key in guess_map:
+            guess_value = guess_map[key]
+            if guess_value > code_value:
+                count += code_value
+            else:
+                count += guess_value
+
+    return count
+
+
+def addOrUpdateDictEntry(letter, dict):
+    if letter in dict:
+        dict[letter] += 1
+    else:
+        dict[letter] = 1
 
 
 def correct_pos_and_color(guess, code):
-    pass
+    count = 0
+    for index in range(4):
+        if guess[index].upper() == code[index]:
+            count += 1
+
+    return count
 
 
 def check_guess(guess, code):
-    pass
+    total_correct = correct_pos_and_color(guess, code)
+    wrong_position = color_count(guess, code) - total_correct
+
+    return total_correct, wrong_position
 
 
 # Wave 3
 def get_win_percentage(wins, plays):
-    pass
+    return int(wins / plays * 100) if plays else 0
 
 
 def format_guess_stats(guess_stats):
-    pass
+    formatted_stats = [""] * 8
+    for num_guesses, wins in guess_stats.items():
+        formatted_stats[num_guesses - 1] = "X" * wins
+
+    return formatted_stats
 
--- app/mastermind.py
+++ app/mastermind.py
@@ -1,6 +1,68 @@
 from .game import generate_code, validate_guess, color_count, correct_pos_and_color, check_guess, check_win_or_lose, get_win_percentage, format_guess_stats
 
-# implement game loop here
+# Implement game loop here
 def mastermind():
-    pass
+    plays = 0
+    wins = 0
+    guess_stats = {}
+    continue_playing = True
+
+    while continue_playing:
+        plays += 1
+        user_guess_count = game_loop()
+        if user_guess_count < 9:
+            wins += 1
+            if user_guess_count in guess_stats:
+                guess_stats[user_guess_count] += 1
+            else:
+                guess_stats[user_guess_count] = 1
+
+        print("Game Stats:")
+        print(f"Total Games: {plays}")
+        print(f"Win percent: {get_win_percentage(wins, plays)}")
+        print("Win Distribution:")
+
+        formatted_win_stats = format_guess_stats(guess_stats)
+        for index in range(8):
+            print(f"{index + 1}: {formatted_win_stats[index]}")
+        print("")
+        
+        play_again = input("Would you like to play again? (y/n): ")
+        continue_playing = play_again == "y"
+    
+def game_loop():
+    print("Generating a 4-letter code...")
+    code = generate_code()
+    print(code)
+    print("Guess the code! Each character in the code is" 
+          + " one of the following letters: R, O, Y, G, B, P")
+    print("")
+    
+    num_guesses = 0
+    while num_guesses <= 8:
+        num_guesses += 1
+        guess = get_valid_guess()
+        
+        if check_win_or_lose(guess, code, num_guesses):
+            print("You won! You guessed the code 🎉")
+            return num_guesses
+
+        guess_info = check_guess(guess, code)
+        print(f"Total colors in the correct locations: {guess_info[0]}")
+        print(f"Total correct colors in the wrong locations: {guess_info[1]}")
+
+    print(f"You ran out of guesses. The code was {code}!")
+    return num_guesses
+
+def get_valid_guess():
+    print("Enter a 4 letter guess:")
+    guess = input("")
+    
+    while not validate_guess(guess):
+        print(f'The guess "{guess}" was invalid')
+        print("Please enter a new guess that is 4 letters long," 
+              + "using only the letters R, O, Y, G, B, and P:")
+        guess = input("")
+
+    return guess
     
