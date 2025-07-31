c0fd909f8725618fc4574f77fa237e25fd1d1a54
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2025-07-31 13:02:15 -0700

    Add generate_code to game.py

--- app/game.py
+++ app/game.py
@@ -1,5 +1,16 @@
+import random
+
 # Wave 1
-# Add your Wave 1 functions here
+# generate_code 
+# - takes no arguments  
+# - returns a list of 4 letters
+# - each letter must be one of: R, O, Y, G, B, P
+# - letters can be repeated
+# - the list is randomly generated
+
+def generate_code():
+    valid_letters = ['R', 'O', 'Y', 'G', 'B', 'P']
+    return [random.choice(valid_letters) for _ in range(4)]
 
 # Wave 2
 # Add your Wave 2 functions here
